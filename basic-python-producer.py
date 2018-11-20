f1d438ec7b07436e34b8d637de0159709e382e4e
Michal Franc <lam.michal.franc@gmail.com>
2018-11-20 21:15:26 +0000

    Basic python producer

---
+++ basic-python-producer.py
@@ -0,0 +1,25 @@
+import boto3
+import uuid
+import logging
+import time
+
+# sqs: 4576
+local_stack_sns_endpoint = 'http://localhost:4575'
+
+local_stack_arn = 'arn:aws:sns:us-east-1:123456789012:'
+
+client = boto3.client(
+    'sns',
+    endpoint_url=local_stack_sns_endpoint,
+    aws_access_key_id='test',
+    aws_secret_access_key='test',
+    aws_session_token='test'
+)
+
+topic_arn = '%stest-producer' % local_stack_arn
+
+while 1:
+    print "... producing message ..."
+    response = client.publish(TopicArn=topic_arn, Message='test message - %s' % uuid.uuid4())
+    print "... message send - response %s" % response
+    time.sleep(1)
